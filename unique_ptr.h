34d381dc226444ec7590e9e003c5482f911a81dc
Preman Jeyabalan <jeyabpre@student.cvut.cz>
2023-06-19 23:55:11 +0800

    added unique ptr base

---
+++ unique_ptr.h
@@ -0,0 +1,12 @@
+
+#ifndef LIB_UNIQUE_PTR
+#define LIB_UNIQUE_PTR
+
+#include <cstdlib>
+#include<iostream>
+
+namespace CustomStd {
+    class unique_ptr {
+        //insert unique pointer implementation here...
+    }
+}

2cad4c0c8f89e424ea87991705f5c6d769810f0b
Preman Jeyabalan <jeyabpre@student.cvut.cz>
2023-06-20 23:42:12 +0800

    added some fun type traits for my own learning, will not be following impl closely

---
+++ type_traits.h
@@ -0,0 +1,46 @@
+#ifndef LIB_TYPE_TRAITS
+#define LIB_TYPE_TRAITS
+
+#include <cstdlib>
+#include <iostream>
+
+namespace CustomStd {
+    template <typename T> 
+    struct is_reference {
+        static const bool value = false;
+    };
+
+    template <typename T> 
+    struct is_reference<T&> {
+        static const bool value = true;
+    };
+
+    template <typename T> 
+    struct is_reference<T&&> {
+        static const bool value = true;
+    };
+
+    template <typename T>
+    inline bool is_reference_v = is_reference<T>::value;
+
+    template <typename T>
+    struct remove_reference {
+        using type = T;
+    };
+
+    template <typename T>
+    struct remove_reference<T&> {
+        using type = T;
+    };
+
+    template <typename T>
+    struct remove_reference<T&&> {
+        using type = T;
+    };
+
+    template <typename T>
+    using remove_reference_t = typename remove_reference<T>::type;
+    
+}
+
+#endif

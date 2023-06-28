59c42ff3d09e189e975288e6c129224934e95f53
Preman Jeyabalan <jeyabpre@student.cvut.cz>
2023-06-28 23:55:49 +0800

    STRING added framework for creating aliaS buffer SSO string

--- src/String.h
+++ src/String.h
@@ -2,11 +2,33 @@
 #define LIB_STRING
 
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
 
 namespace CustomStd {
     class string {
-        //insert sso string implementation here...
+    public:
+        string() : _short.size_flag(0) {
+            _short.buffer[0] = '\0';
+        }
+
+
+    private:
+        struct long_t {
+            size_t capacity, size;
+            char* data;
+        };
+
+        static constexpr size_t _short_capacity = sizeof(long_t) - 2;
+
+        struct short_t {
+            unsigned char size_flag;
+            char buffer[_short_capacity + 1];
+        };
+
+        union {
+            long_t _long;
+            short_t _short;
+        };
     };
 }
 
---
+++ tests/string.cpp
@@ -0,0 +1,14 @@
+//
+// Created by preman on 6/28/23.
+//
+#include <gtest/gtest.h>
+#include <String.h>
+
+
+struct StringTest : public testing::Test {
+
+};
+
+TEST_F(StringTest, foo) {
+    CustomStd::string k;
+}
